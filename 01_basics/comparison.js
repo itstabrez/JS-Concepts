68280c47aa0ad4cd48e20690f2237d8da93a5e36
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-03-23 11:52:30 +0000

    Comparison in JS

---
+++ 01_basics/comparison.js
@@ -0,0 +1,18 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+
+// Some Complex Comparison
+console.log("2">1)      //TRUE O/P
+console.log("02">1)     //TRUE O/P
+console.log(null>2)     //FALSE O/P
+console.log(null==2)    //FALSE O/P
+console.log(null>=2)    //FALSE O/P
+
+console.log(undefined>2)     //FALSE O/P
+console.log(undefined==2)    //FALSE O/P
+console.log(undefined>=2)    //FALSE O/P
+
+console.log("2"===2)    //FALSE O/P as === operartor compare data type too
