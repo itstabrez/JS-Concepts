50f0d335918c18a6f1b16e7473c5d44580f133f4
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-03-23 10:45:49 +0000

    data types and variables

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer JS
+
+// alert(3+3);   //we are using nodeJs not browser
+console.log(3 + 3);
+
+console.log("TabreZ");
+
+let name="Tabrez";
+let age=19;
+let isStudent=true;
+
+// Data Types
+// null
+// int
+// bigint
+// undefined
+// boolean
+// symbol
+
+
+// object
+
+console.log(typeof age)
+console.log("NULL TYPE IS OBJECT" ,typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const randomNumber = 144553;   //This can't be updated later
+let mailID = "tabrezamrath@gmail.com";
+var randomText= "nothing";
+accountCity = "Jamui";
+
+console.log(randomNumber);
+console.table([randomNumber,randomText,mailID,accountCity]);
