a82e7f62fd4f23431dbfc79b5a89748024ecd3e0
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-03-24 18:07:40 +0000

    Functions in JS and handling objects with functions

---
+++ 03_basics/1Function.js
@@ -0,0 +1,74 @@
+function myName() {
+    console.log("T");
+    console.log("A");
+    console.log("B");
+    console.log("R");
+    console.log("E");
+    console.log("Z");
+}
+
+myName();
+
+// function addTwoNum(number1,number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNum(number1,number2){
+    return number1 + number2;
+}
+
+const result = addTwoNum(4,8);
+// addTwoNum(4,7);
+
+console.log(result);
+
+function userLoginMessage(username = 'Hola'){
+    if(!username){
+        console.log("Please Enter a username");
+        return;
+    }
+    return `${username} just logged in`
+}
+
+console.log(userLoginMessage("Tabrezzzz"));
+console.log(userLoginMessage());
+
+//IF WE DON'T KNOW HOW MANY ARGUMENTS WILL BE PASSED WHILE CALLING A FUNCTION THEN WE USE REST OPERATOR
+
+function calculateCartItemPrice(...num1) {     //  '  ... ' is known as rest operator also spread operator depends on use cases
+    return num1;                               
+}         
+
+console.log(calculateCartItemPrice(2 , 7 , 10));    
+
+
+// HANDLING OBJECTS IN FUNCTIONS 
+
+const myObj = {
+    name: "Tabrez",
+    age: 20
+}
+
+function handleObject(anyObject){ 
+    console.log(`User Name is ${anyObject.name} and age is ${anyObject.age}`);
+}
+
+handleObject(myObj);
+
+handleObject({
+    name: "Tom",
+    age: 22
+})
+
+// HANDLING ARRAYS IN FUNCTION
+
+const myNewArray = [200 , 500 , "Anonymus"]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100 , 300 , 200]));
+
+
