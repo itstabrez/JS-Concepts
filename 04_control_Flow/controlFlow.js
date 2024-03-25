602e257076ba98b95ae10a99355fca83122152fd
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-03-25 20:33:58 +0000

    Control Flow , This Keyword, Arrow Function and IIFE in JS

---
+++ 04_control_Flow/controlFlow.js
@@ -0,0 +1,23 @@
+const isUserLoggedIn = true;
+
+if (isUserLoggedIn) {
+    console.log("Executed");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+let val1;
+val1 = 5 ?? 10;
+val1 = null ?? 15;
+
+val1 = null ?? 15 ?? 18;
+
+let var2 = undefined ?? 14;
+
+
+console.log(val1);
+console.log(var2);
+
+// Terniary Operator ?
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("Less Than 80") : console.log("More Than 80");
