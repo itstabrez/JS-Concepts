92b33d57e901351fc0ae993a9ef7afd8bfbefcdd
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-03-29 20:00:10 +0000

    Asuync in JS

---
+++ Async/async.js
@@ -0,0 +1,9 @@
+// BLOCKING CODE --- This will block the execution of program while reading file
+
+
+// NON BLOCKING CODE -- This will allow the execution of program and parallely read the file 
+
+// ASYNCHRONOUS -- Asynchronous programming is a technique that enables your program to start a potentially long-running task and still be able to be responsive to other events while that task runs, rather than having to wait until that task has finished. Once that task has finished, your program is presented with the result
+
+// Example -- Time Out Function etc;
+
