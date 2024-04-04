f481ba2410e9cd2d17d1de85a8077d387f1f9245
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-04-04 23:01:46 +0530

    Update variables.js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,6 +1,6 @@
 const randomNumber = 144553;   //This can't be updated later
 let mailID = "tabrezamrath@gmail.com";
-var randomText= "nothing";
+var randomText= "nothing";   // This is an old syntax which was used earlier but nowadays because of some limitations it is not used 
 accountCity = "Jamui";
 
 console.log(randomNumber);
