69f3ffec3387fefd3305391779792149a509b2d4
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-04-07 17:43:53 +0000

    Binding basics

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -1,6 +1,6 @@
 const id = Symbol('123')
 const anotherId  = Symbol('123')
-console.log(id===anotherId)
+console.log(id===anotherId)     //FALSE O/P because because symbol is unique in itself
 
 // ARRAYS, OBJECTS, Functions
 const array = ["Shaktiman", "ChotaBheem", "Krrish"]
