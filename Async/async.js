39ea46a54b01b42072842a27298042e0e535e2f7
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-04-08 18:00:44 +0000

    Async in Js

--- Async/async.js
+++ Async/async.js
@@ -5,5 +5,5 @@
 
 // ASYNCHRONOUS -- Asynchronous programming is a technique that enables your program to start a potentially long-running task and still be able to be responsive to other events while that task runs, rather than having to wait until that task has finished. Once that task has finished, your program is presented with the result
 
-// Example -- Time Out Function etc;
+// Example -- Time Out Function , Set Time Interval etc;
 
